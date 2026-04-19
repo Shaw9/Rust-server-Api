63ef38aef350f3d9cb8e435b23993f71d08584f3
Shaw <m1295301019@163.com>
2026-04-19 19:59:31 +0800

    1.0.0

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -1,5 +1,4 @@
 pub mod send_notification;
-
 pub use send_notification::{send_notification, NotificationOptions, NotificationResult};
 pub mod get_windows_info;
 pub use get_windows_info::{get_windows_info, WindowsInfo};
